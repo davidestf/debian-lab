11ec04c96748f2c33a5e79a96ee974b037439130
Davide staffieri <69149868+dstf@users.noreply.github.com>
2021-05-29 10:22:41 +0100

    Create init-script.sh

---
+++ init-script.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+HOSTNAME=$(hostname)
+
+export DEBIAN_FRONTEND=noninteractive
+
+apt update && apt upgrade
+
+apt-get install -y \
+curl \
+dnsutils \
+git \
+nginix \
+net-tools \
+ufw 
+
+ufw default deny incoming
+ufw default allow outgoing
+
+ufw allow ssh/tcp
+ufw allow http/tcp
+ufw allow https/tcp
+
+ufw enable
+
+rm /var/www/html/index.nginx-debian.html
+rm /etc/nginx/sites-enabled/default
+touch /var/www/html/index.html
+
+cat <<END >/etc/nginx/sites-enabled/default
+server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+
+        root /var/www/html;
+
+        # Add index.php to the list if you are using PHP
+        index index.html index.htm index.nginx-debian.html;
+
+        server_name ${HOSTNAME};
+}
+END
+
