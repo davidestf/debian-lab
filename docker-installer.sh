d402c57cd189a909132350c901d3fe4dc5c45b70
Davide staffieri <davide@protoweb.me>
2021-10-29 23:41:37 +0100

    Update and rename docker to docker-installer.sh

--- docker-installer.sh
+++ docker-installer.sh
@@ -10,8 +10,8 @@ apt-get install \
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+"deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt-get update
 apt-get install docker-ce docker-ce-cli containerd.io
