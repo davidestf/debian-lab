ceeb62876075e51d7fb46b9da8cf0224fce91198
Davide staffieri <davide@protoweb.me>
2021-11-04 00:44:40 +0000

    Update python2.sh

--- python2.sh
+++ python2.sh
@@ -1,6 +1,6 @@
 ### Python2
-apt install python
+sudo apt install python
 
 sudo apt install curl
-curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
+sudo curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
 sudo python2 get-pip.py
