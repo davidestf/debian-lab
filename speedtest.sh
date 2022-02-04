f463b62e99e578b741922bdf7f1ab91bdabb7994
Davide staffieri <davide@protoweb.me>
2022-02-04 14:25:02 +0000

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python
+
+
+
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/dstf/debian-lab/main/speedtest.sh)"
+
