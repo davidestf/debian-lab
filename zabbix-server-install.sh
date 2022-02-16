016542a1f7876d2a51257507a8e69c068fa1c072
Davide staffieri <davide@protoweb.me>
2022-02-16 12:36:16 +0000

    Update zabbix-server-install.sh

--- zabbix-server-install.sh
+++ zabbix-server-install.sh
@@ -9,13 +9,13 @@ apt install -y zabbix-server-mysql zabbix-frontend-php zabbix-nginx-conf zabbix-
 sed -i 's/^# *\(en_US.UTF-8\)/\1/' /etc/locale.gen
 locale-gen
 #dpkg-reconfigure locales
-echo "Mot de passe de l'utilisateur mysql zabbix"
+echo "User Password mysql zabbix"
 read -p "Password for mysql user zabbix:
 " password
 echo "create database zabbix character set utf8 collate utf8_bin;" | mysql
 echo "create user zabbix@localhost identified by '$password';" | mysql
 echo "grant all privileges on zabbix.* to zabbix@localhost;" | mysql
-echo "Entrez à nouveau votre mot de passe récemment créé"
+echo "Enter again your password recently created"
 echo "Enter again your password recently created"
 zcat /usr/share/doc/zabbix-sql-scripts/mysql/server.sql.gz | mysql -uzabbix -p zabbix
 sed -i "s/# DBPassword=/DBPassword\=$password/" /etc/zabbix/zabbix_server.conf
