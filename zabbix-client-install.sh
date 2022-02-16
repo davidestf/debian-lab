ccf7be3c973c5254655386393818959162c436e0
Davide staffieri <davide@protoweb.me>
2022-02-16 12:44:02 +0000

    Create zabbix-client-install.sh

---
+++ zabbix-client-install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd /tmp
+wget https://repo.zabbix.com/zabbix/6.0/debian/pool/main/z/zabbix-release/zabbix-release_6.0-1+debian11_all.deb
+dpkg -i zabbix-release_6.0-1+debian11_all.deb
+apt update
+apt upgrade -y
+apt install zabbix-agent -y
+read -p "What is Zabbix server address ?
+" host
+sed -i "s/Server=127.0.0.1/Server\=$host/" /etc/zabbix/zabbix_agentd.conf
+systemctl restart zabbix-agent.service
