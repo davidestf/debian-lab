1dd20832ff871c85f46278a775c1fe81cfe92fe7
Davide staffieri <davide@protoweb.me>
2022-02-18 19:22:41 +0000

    Update stress-ng.sh

--- stress-ng.sh
+++ stress-ng.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 apt install stress-ng
 
 
@@ -7,3 +9,6 @@ stress-ng --vm-bytes $(awk '/MemFree/{printf "%d\n", $2 * 0.9;}' < /proc/meminfo
 #use MemAvailable instead to estimate available memory for new processes without swapping:
 stress-ng --vm-bytes $(awk '/MemAvailable/{printf "%d\n", $2 * 0.9;}' < /proc/meminfo)k --vm-keep -m 1
 
+
+
+# sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/dstf/debian-lab/main/stress-ng.sh)"
