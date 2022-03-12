2acd8f9dab748109b8e5de4a26b876e16309bbee
Davide staffieri <davide@protoweb.me>
2022-03-12 21:42:26 +0000

    Create plesk-server-install.sh

---
+++ proxmox/plesk-server-install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+ 
+apt install gpg apt-transport-https curl wget -y
+ 
+#wget https://downloads.plex.tv/plex-media-server-new/1.25.6.5577-c8bd13540/debian/plexmediaserver_1.25.6.5577-c8bd13540_amd64.deb
+#dpkg -i plexmediaserver*
+#dpkg -L plexmediaserver
+#sudo nano /etc/apt/sources.list.d/plexmediaserver.list
+#apt update
+
+
+
+
+
+#SSH tunnel
+#ssh ip.address.of.server -L 8888:localhost:32400
+#http://localhost:8888/web
