76bc00b79a2b63590b1b52bbae2d27146143dc1e
Davide staffieri <davide@protoweb.me>
2022-07-12 15:07:48 +0100

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -24,7 +24,7 @@ apt-get install docker-ce docker-ce-cli containerd.io
 
 
 
-curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
